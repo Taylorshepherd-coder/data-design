17959b86397ea12077cbb946dd4232486ed4cebb
Taylor Shepherd <tshepherd1@cnm.edu>
2020-03-11 10:40:03 -0600

    I deleted an exclamation mark at the end of Marie's quote

---
+++ Epic/persona.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mary May</title>
+</head>
+<h1>
+	Marie May
+</h1>
+<p>
+	"Makeup is a women's best friend" -Mary May
+<p/>
+<p>
+	Marie May is a 24 year woman from Albuquerque, New Mexico who loves to watch videos on makeup tutorials weekly to keep up with new style trends. She watches videos to learn new styles via Youtube and she watches them on her Andriod phone and her laptop. 	She isn't new to makeup but she hasn't gone to beauty school either to get a degree, she is all self-taught. Marie keeps up with the newest makeup styles and trends and tries to recreate those styles on herself and her friends. She would be considered what her friends call her a "professional makeup artist" simply because of all the videos she watches and the time she spends watching them, and the time she spends actually practicing these styles on herself and her friends. She likes to follow certain Youtubers who mainly recreate the newest trends in makeup especially those who also use her favorite brands of makeup such as Chanell, Mac and Bare Minerals. She freelances on the side, so this is not only her passion but she is able to make some money on the side out of it as well. She has been looking into going to school for cosutume design makeup to do makeup for those that are in movies.
+</p>
+
+<body>
+
+
+</body>
+</html>
+
---
+++ Epic/user-story.php
@@ -0,0 +1,1 @@
+As a YouTube user, Marie May wants to watch videos on makeup tutorials to increase her knowledge and ability to recreate the latest makeup trends.
